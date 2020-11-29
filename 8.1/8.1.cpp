79712060109b0f535e410eee3856fdee9b3f203f
BGBro <bohdan.horbachov.itis.2020@lpnu.ua>
2020-11-30 00:23:53 +0200

    master

---
+++ 8.1/8.1.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int Count(const string s)
+{
+	int k = 0;
+	size_t pos = 0;
+	int i = 0;
+	for(i; i <= s.length(); i++)
+		if (s[i] == ',')
+		{
+			k++;
+			if (k == 3)
+			{
+				return i;
+				break;
+			}
+		}
+}
+
+string Change(string& s)
+{
+	size_t pos = 0;
+	while ((pos = s.find(',', pos)) != -1)
+		s.replace(pos, 1, "**");
+	return s;
+}
+
+int main()
+{
+	string str;
+	cout << "Enter string:" << endl;
+	getline(cin, str);
+	cout << "Index of third ',' is " << Count(str) << endl;
+	string dest = Change(str);
+	cout << "Modified string (param) : " << str << endl;
+	cout << "Modified string (result): " << dest << endl;
+	system("pause");
+	return 0;
+}
---
+++ UnitTest 8.1/UnitTest 8.1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../8.1/8.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest81
+{
+	TEST_CLASS(UnitTest81)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t;
+			t = Count(",,,");
+			Assert::AreEqual(t, 2);
+		}
+	};
+}
